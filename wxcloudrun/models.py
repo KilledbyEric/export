c0a64e8277db4c85c8a18917189abfd4ffedb67b
KilledbyEric <Epattinson@163.com>
2022-12-10 22:37:44 +0800

    update

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -3,15 +3,37 @@ from datetime import datetime
 from django.db import models
 
 
-# Create your models here.
-class Counters(models.Model):
-    id = models.AutoField
-    count = models.IntegerField(max_length=11, default=0)
-    createdAt = models.DateTimeField(default=datetime.now(), )
-    updatedAt = models.DateTimeField(default=datetime.now(),)
+# # Create your models here.
+# class Counters(models.Model):
+#     id = models.AutoField
+#     count = models.IntegerField(max_length=11, default=0)
+#     createdAt = models.DateTimeField(default=datetime.now(), )
+#     updatedAt = models.DateTimeField(default=datetime.now(),)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
+
+#     class Meta:
+#         db_table = 'Counters'  # 数据库表名
+
+
+class exportdata(models.Model):
+    id = models.CharField(max_length=20, null=False, blank=False,primary_key=True)
+    collageName = models.CharField(max_length=100, null=False, blank=False)
+    majorName = models.CharField(max_length=100, null=False, blank=False)
+    note = models.CharField(max_length=800, null=True, blank=True)
+    schooling = models.SmallIntegerField(max_length=5, null=False, blank=False)
+    province = models.CharField(max_length=10, null=False, blank=False)
+    city = models.CharField(max_length=10, null=False, blank=False)
+    bz = models.CharField(max_length=4, null=False, blank=False)
+    tuition = models.CharField(max_length=10, null=False, blank=False)
+    xk = models.CharField(max_length=20, null=False, blank=False)
+    planNum = models.SmallIntegerField(max_length=4, null=False, blank=False)
+    score = models.SmallIntegerField(max_length=3, null=False, blank=False)
+    position = models.IntegerField(max_length=7, null=False, blank=False)
+    CDR = models.CharField(max_length=2, null=True, blank=True)
+    level = models.CharField(max_length=10, null=True, blank=True)
+    owner = models.CharField(max_length=10, null=True, blank=True)
 
     class Meta:
-        db_table = 'Counters'  # 数据库表名
+        db_table = 'exportdata'  # 数据库表名
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,89 +1,144 @@
 import json
 import logging
+import xlsxwriter
+from datetime import datetime
+from wxcloudrun import settings
 
-from django.http import JsonResponse
+from django.http import FileResponse
 from django.shortcuts import render
-from wxcloudrun.models import Counters
+from wxcloudrun.models import exportdata
+import os
 
 
 logger = logging.getLogger('log')
 
 
-def index(request):
+def index(request,_):
     if request.method == 'GET' or request.method == 'get':
         return render(request, 'index.html')
     else:
-        list = request.POST.get("list")
-        print(list)
+        timeStamp = datetime.now().strftime('%Y%m%d%H%M%S%f')
+        file_name = "志愿表"+timeStamp+".xlsx"
+        dest_filename = str(settings.BASE_DIR) + '\\doc_tmp\\' + file_name
+        # dest_filename = '.' + '\\doc_tmp\\' + file_name
+        wb = xlsxwriter.Workbook(dest_filename)
+        ws = wb.add_worksheet('志愿表')
+        columns = ['院校代码', '院校名称', '专业代码', '专业名称', '专业简注',  '省份', '城市', '本专', '学制', '学费', '2022选考科目', '2022计划数', '2022投档线', '2022投档位次', '第四轮学科评估结果', '办学特色', '办学性质']
+                    # 0          1           2           3         4          5        6     7       8       9         10            11             12             13               14               15         16               
+        row_num = 0
+        for col_num in range(len(columns)):
+            ws.write(row_num, col_num, columns[col_num])
+            
+        dataList = request.POST.get('list').split(",")
+        rows = exportdata.objects.filter(id__in = dataList).values_list()
+        # ['wishData0001003', '浙江大学(一流大学建设高校)', '外国语言文学类', '含英语、翻译专业。', 4, '浙江', '杭州', '本科', '5300', '不限', 10, 665, 3044, 'A', '985', '公办']
+        #              0                 1                      2                    3          4    5      6        7      8      9      10  11    12    13   14    15                            
+        for row in rows:
+            row_num += 1
+            row = list(row)
+            # print(row)
+            for col_num in range(len(row)):
+                ws.write(row_num, 0, row[0][-7:-3])
+                ws.write(row_num, 1, row[1])
+                ws.write(row_num, 2, row[0][-3:])
+                ws.write(row_num, 3, row[2])
+                ws.write(row_num, 4, row[3])
+                ws.write(row_num, 5, row[5])
+                ws.write(row_num, 6, row[6])
+                ws.write(row_num, 7, row[7])
+                ws.write(row_num, 8, row[4])
+                ws.write(row_num, 9, row[8])
+                ws.write(row_num, 10, row[9])
+                ws.write(row_num, 11, row[10])
+                ws.write(row_num, 12, row[11])
+                ws.write(row_num, 13, row[12])
+                ws.write(row_num, 14, row[13])
+                ws.write(row_num, 15, row[14])
+                ws.write(row_num, 16, row[15])
+        # data = exportdata.objects.filter(id__in=list)
+        # for item in data:
+        #     print(item)
+        #     print('aaa')
+        # data = serializers.serialize("json", exportdata.objects.filter(id=list[0]))
+        # print(data)
+    wb.close()
+    excel = open(dest_filename,"rb")
+    # FileResponse 该类可以将文件下载到浏览器
+    response = FileResponse(excel)
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file_name)
+    os.remove(dest_filename)
+    return response
+
+ 
+
+# def counter(request, _):
+#     """
+#     获取当前计数
+
+#      `` request `` 请求对象
+#     """
+
+#     rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
+#     if request.method == 'GET' or request.method == 'get':
+#         rsp = get_count()
+#     elif request.method == 'POST' or request.method == 'post':
+#         rsp = update_count(request)
+#     else:
+#         rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
+#                             json_dumps_params={'ensure_ascii': False})
+#     logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
+#     return rsp
 
 
-def counter(request, _):
-    """
-    获取当前计数
+# def get_count():
+#     """
+#     获取当前计数
+#     """
 
-     `` request `` 请求对象
-    """
-
-    rsp = JsonResponse({'code': 0, 'errorMsg': ''}, json_dumps_params={'ensure_ascii': False})
-    if request.method == 'GET' or request.method == 'get':
-        rsp = get_count()
-    elif request.method == 'POST' or request.method == 'post':
-        rsp = update_count(request)
-    else:
-        rsp = JsonResponse({'code': -1, 'errorMsg': '请求方式错误'},
-                            json_dumps_params={'ensure_ascii': False})
-    logger.info('response result: {}'.format(rsp.content.decode('utf-8')))
-    return rsp
+#     try:
+#         data = Counters.objects.get(id=1)
+#     except Counters.DoesNotExist:
+#         return JsonResponse({'code': 0, 'data': 0},
+#                     json_dumps_params={'ensure_ascii': False})
+#     return JsonResponse({'code': 0, 'data': data.count},
+#                         json_dumps_params={'ensure_ascii': False})
 
 
-def get_count():
-    """
-    获取当前计数
-    """
+# def update_count(request):
+#     """
+#     更新计数，自增或者清零
 
-    try:
-        data = Counters.objects.get(id=1)
-    except Counters.DoesNotExist:
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    return JsonResponse({'code': 0, 'data': data.count},
-                        json_dumps_params={'ensure_ascii': False})
+#     `` request `` 请求对象
+#     """
 
+#     logger.info('update_count req: {}'.format(request.body))
 
-def update_count(request):
-    """
-    更新计数，自增或者清零
+#     body_unicode = request.body.decode('utf-8')
+#     body = json.loads(body_unicode)
 
-    `` request `` 请求对象
-    """
+#     if 'action' not in body:
+#         return JsonResponse({'code': -1, 'errorMsg': '缺少action参数'},
+#                             json_dumps_params={'ensure_ascii': False})
 
-    logger.info('update_count req: {}'.format(request.body))
-
-    body_unicode = request.body.decode('utf-8')
-    body = json.loads(body_unicode)
-
-    if 'action' not in body:
-        return JsonResponse({'code': -1, 'errorMsg': '缺少action参数'},
-                            json_dumps_params={'ensure_ascii': False})
-
-    if body['action'] == 'inc':
-        try:
-            data = Counters.objects.get(id=1)
-        except Counters.DoesNotExist:
-            data = Counters()
-        data.id = 1
-        data.count += 1
-        data.save()
-        return JsonResponse({'code': 0, "data": data.count},
-                    json_dumps_params={'ensure_ascii': False})
-    elif body['action'] == 'clear':
-        try:
-            data = Counters.objects.get(id=1)
-            data.delete()
-        except Counters.DoesNotExist:
-            logger.info('record not exist')
-        return JsonResponse({'code': 0, 'data': 0},
-                    json_dumps_params={'ensure_ascii': False})
-    else:
-        return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
-                    json_dumps_params={'ensure_ascii': False})
+#     if body['action'] == 'inc':
+#         try:
+#             data = Counters.objects.get(id=1)
+#         except Counters.DoesNotExist:
+#             data = Counters()
+#         data.id = 1
+#         data.count += 1
+#         data.save()
+#         return JsonResponse({'code': 0, "data": data.count},
+#                     json_dumps_params={'ensure_ascii': False})
+#     elif body['action'] == 'clear':
+#         try:
+#             data = Counters.objects.get(id=1)
+#             data.delete()
+#         except Counters.DoesNotExist:
+#             logger.info('record not exist')
+#         return JsonResponse({'code': 0, 'data': 0},
+#                     json_dumps_params={'ensure_ascii': False})
+#     else:
+#         return JsonResponse({'code': -1, 'errorMsg': 'action参数错误'},
+#                     json_dumps_params={'ensure_ascii': False})
